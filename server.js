e1e621a449b8f583ae7b9ae07abab7f158430ed0
Daniel Smith <daniel.smith8795@gmail.com>
2019-12-13 10:26:02 -0600

    trying to add in localhost checklist again

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 // Connect to the Mongo DB
 //If using Mongo for deployment leave as process.env.MONGODB_URI
 //if running on local server set as || "mongodb://localhost/checklist"
-var MONGODB_URI = process.env.MONGODB_URI;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/checklist";
 mongoose.connect(MONGODB_URI);
 
 // Define API routes 
