48f5d8ba0d75c383440e9fb3085139b6f2ef6a0f
Daniel Smith <daniel.smith8795@gmail.com>
2019-12-17 15:12:26 -0600

    style changes and incorporated moment in to all devices page

--- client/src/components/viewAllDevices.js
+++ client/src/components/viewAllDevices.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Moment from "react-moment";
 
 const ViewAll = ({inventoryNum,dateBought,assigned,assignedTo, workGroup, location,_id,dataPlan,type,notes}) => {
     const nullResponse = function(data){
@@ -15,7 +15,7 @@ const ViewAll = ({inventoryNum,dateBought,assigned,assignedTo, workGroup, locati
             <tbody className="deviceData">
                 <tr>
                     <th scope="row"><a href={"/deviceID/"+_id}> {inventoryNum} </a></th>
-                    <td> {dateBought} </td>
+                    <td><Moment format="MM/DD/YYYY">{dateBought}</Moment></td>
                     <td> {workGroup} </td>
                     <td> {assignedTo} </td>
                     <td> {nullResponse(location)}</td>
